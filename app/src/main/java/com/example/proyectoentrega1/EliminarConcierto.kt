073d257eeaeec3bf021fb7fae7f5aae88dc68079
TommyDS2005 <116994238+TommyDS2005@users.noreply.github.com>
2024-04-17 11:39:52 -0500

    Cambios proyecto 2

--- app/src/main/java/com/example/proyectoentrega1/EliminarConcierto.kt
+++ app/src/main/java/com/example/proyectoentrega1/EliminarConcierto.kt
@@ -2,11 +2,15 @@ package com.example.proyectoentrega1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import org.json.JSONArray
+import org.json.JSONObject
 
 class EliminarConcierto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +18,14 @@ class EliminarConcierto : AppCompatActivity() {
         setContentView(R.layout.activity_eliminar_concierto)
         val conciertosString = intent.getStringExtra("conciertos")
         val conciertosJSONarray = JSONArray(conciertosString)
+        //val conciertoString = intent.getStringExtra("concierto")
+        //val concierto = JSONObject(conciertoString)
         val btnEliminarConcierto = findViewById<Button>(R.id.eliminar)
 
+        val fechaConcierto = findViewById<TextView>(R.id.fechaConcierto)
+        val ciudadConcierto = findViewById<TextView>(R.id.ciudadConcierto)
+        val lugarConcierto = findViewById<TextView>(R.id.lugarConcierto)
+        val horaConcierto = findViewById<TextView>(R.id.horaConcierto)
 
         val conciertos = convertJsonArrayToList(conciertosJSONarray)
 
@@ -27,7 +37,8 @@ class EliminarConcierto : AppCompatActivity() {
 
         btnEliminarConcierto.setOnClickListener {
             val conciertoSeleccionado = spinner.selectedItem.toString()
-            val concierto = conciertosJSONarray.getJSONObject(conciertos.indexOf(conciertoSeleccionado))
+            val concierto =
+                conciertosJSONarray.getJSONObject(conciertos.indexOf(conciertoSeleccionado))
             conciertosJSONarray.remove(conciertos.indexOf(conciertoSeleccionado))
             val intent = intent
             intent.putExtra("conciertos", conciertosJSONarray.toString())
@@ -35,7 +46,32 @@ class EliminarConcierto : AppCompatActivity() {
             Toast.makeText(this, "Evento eliminado", Toast.LENGTH_SHORT).show()
         }
 
+        val adapter2 = ArrayAdapter(this, android.R.layout.simple_spinner_item, conciertos)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
+        // Asignar el adaptador al Spinner
+        spinner.adapter = adapter2
+
+        // Establecer un listener para cuando un ítem es seleccionado
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                // Asumiendo que cada concierto en el JSONArray tiene los campos: fecha, ciudad, centro_de_eventos, hora, etc.
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                val concierto = conciertosJSONarray.getJSONObject(position)
+
+                // Actualizar los TextViews con los datos del concierto seleccionado
+                fechaConcierto.text = concierto.getString("ciudad")
+                ciudadConcierto.text = concierto.getString("centro_de_eventos")
+                lugarConcierto.text = concierto.getString("fecha")
+                horaConcierto.text = "Hora: ${concierto.getString("hora")}"
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Este método se invoca cuando la selección desaparece de este view
+            }
+        }
+
+    }
     }
     fun convertJsonArrayToList(jsonArray: JSONArray): ArrayList<String> {
         val listData = ArrayList<String>()
@@ -45,5 +81,3 @@ class EliminarConcierto : AppCompatActivity() {
         }
         return listData
     }
-
-}
--- app/src/main/java/com/example/proyectoentrega1/MenuAdmin.kt
+++ app/src/main/java/com/example/proyectoentrega1/MenuAdmin.kt
@@ -3,18 +3,26 @@ package com.example.proyectoentrega1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
+import org.json.JSONArray
 
 class MenuAdmin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_admin)
 
+        val listaConciertos = findViewById<ListView>(R.id.listViewAdmon)
         val btnAgregar = findViewById<Button>(R.id.btnAgregar)
         val btnEliminar = findViewById<Button>(R.id.btnEliminar)
         val conciertosString = intent.getStringExtra("conciertos")
+        val conciertos = JSONArray(conciertosString)
 
+        val nombresConciertos = obtenerNombresConciertos(conciertos)
 
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, nombresConciertos)
+        listaConciertos.adapter = adapter
 
         btnAgregar.setOnClickListener {
             val intent = Intent(this, AgregarConcierto::class.java)
@@ -26,5 +34,22 @@ class MenuAdmin : AppCompatActivity() {
             intent.putExtra("conciertos", conciertosString)
             startActivity(intent)
         }
+
+        val intent = Intent(this, detallesConcierto::class.java)
+
+        listaConciertos.setOnItemClickListener { parent, view, position, id ->
+            val concierto = conciertos.getJSONObject(position)
+            intent.putExtra("concierto", concierto.toString())
+            startActivity(intent)
+        }
+    }
+
+    private fun obtenerNombresConciertos(destinos: JSONArray): MutableList<String> {
+        val nombresConciertos = mutableListOf<String>()
+        for (i in 0 until destinos.length()) {
+            val conciertos = destinos.getJSONObject(i)
+            nombresConciertos.add(conciertos.getString("artista") + " " + conciertos.getString("ciudad"))
+        }
+        return nombresConciertos
     }
 }
